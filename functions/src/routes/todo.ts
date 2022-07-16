0b9a3a5cb8b5fb82c82d237ee3f6434d71a884e3
0seo8 <96939562+0seo8@users.noreply.github.com>
2022-07-16 22:52:54 +0900

    create fb-test

---
+++ functions/src/routes/todo.ts
@@ -0,0 +1,114 @@
+import * as express from 'express'
+import * as admin from 'firebase-admin'
+import {saveFile} from '../utils'
+
+const router = express.Router()
+const db = admin.firestore()
+
+interface Todo {
+  id?: string
+  title: string,
+  image?: string | null ,
+  done: boolean,
+  createdAt: string,
+  updatedAt: string,
+  deleted: boolean
+}
+
+router.get('/', async (req, res) => {
+  const snaps =  await db.collection('Todos')
+  .where('deleted', '!=', true)
+  .get()
+  
+  const todos: Todo[] = []
+  snaps.forEach(snap => {
+    const fields = snap.data()
+
+    todos.push({
+      id: snap.id,
+      ...fields as Todo
+    })
+  })
+
+  //최신일 정렬
+  todos.sort((a,b) => {
+    const aTime : number = new Date(a.createdAt).getTime()
+    const bTime : number = new Date(b.createdAt).getTime()
+    return bTime-aTime
+  })
+
+  res.status(200).json(todos)
+})
+
+//todo추가
+router.post('/', async (req, res) => {
+  const {title, imageBase64} = req.body
+  const date = new Date().toISOString()
+
+  const image = await saveFile(imageBase64)
+
+  const todo: Todo = {
+    title,
+    image,
+    done: false,
+    createdAt: date,
+    updatedAt: date,
+    deleted: false
+  }
+
+  const ref = await db.collection('Todos').add(todo)
+
+  res.status(200).json({
+    id: ref.id,
+    ...todo
+  })
+})
+
+//todo 수정
+router.put('/:id', async(req, res) => {
+  const {title, done, imageBase64} = req.body 
+  const {id} = req.params
+
+  const snap = await db.collection('Todos').doc(id).get()
+  if(!snap.exists) {
+    return res.status(404).json('존재하지 않는 정보입니다.')
+  }
+
+  const image = await saveFile(imageBase64)
+
+  const {createdAt} = snap.data() as Todo
+  const updatedAt = new Date().toDateString
+
+  await snap.ref.update({ //이것은 db를 갱신해주는 것이지 snap을 갱신해주는 것은 아닙니다.
+    title,
+    image,
+    done,
+    updatedAt: new Date().toDateString()
+  })
+
+  return res.status(200).json({
+    id: snap.id,
+    title,
+    image,
+    done,
+    createdAt,
+    updatedAt
+  })
+})
+
+router.delete('/:id', async(req, res) => {
+  const { id } = req.params
+  const snap = await db.collection('Todos').doc(id).get()
+
+  if(!snap.exists) {
+    return res.status(404).json('존재하지 않는 정보입니다.')
+  }
+
+  await snap.ref.update({
+    deleted: true,
+  })
+
+  return res.status(200).json(true)
+})
+
+export default router

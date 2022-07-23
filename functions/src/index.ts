a917f765cbc5a9ae62df84c5fcd03cc36f476928
0seo8 <96939562+0seo8@users.noreply.github.com>
2022-07-23 18:28:04 +0900

    feat: cors 모두 허용

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -10,13 +10,15 @@ import todo from './routes/todo'
 
 const app = express()
 app.use(express.json())
-app.use(cors({
-  origin: [
-    'http://localhost:3000',
-    'kdt-test-97b7e.web.app', 
-    'kdt-test-97b7e.firebaseapp.com'
-  ]
-}))
+app.use(cors(
+//   {
+//   origin: [
+//     'http://localhost:3000',
+//     'kdt-test-97b7e.web.app', 
+//     'kdt-test-97b7e.firebaseapp.com'
+//   ]
+// }
+))
 app.use('/todo', todo)
 
 export const api = functions
